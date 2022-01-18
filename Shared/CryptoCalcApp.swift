215bb9ee0bac03f8e7e29bc234b7950c8e6c9f28
Artem Karma <inste@icloud.com>
2022-01-18 11:30:02 +0200

    Initial Commit

---
+++ Shared/CryptoCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCalcApp.swift
+//  Shared
+//
+//  Created by Артём Кармазь on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
